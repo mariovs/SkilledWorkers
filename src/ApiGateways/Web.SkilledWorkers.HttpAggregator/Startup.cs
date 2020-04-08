d48a328c38b2e90b05e809fa48b87d656e31fe84
Mario Vasile <ma_va@coreledger.net>
2020-04-08 20:32:33 +0200

    Swagger added for Api Gateway

--- src/ApiGateways/Web.SkilledWorkers.HttpAggregator/Startup.cs
+++ src/ApiGateways/Web.SkilledWorkers.HttpAggregator/Startup.cs
@@ -1,9 +1,13 @@
+using System;
+using System.IO;
+using System.Reflection;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Web.SkilledWorkers.HttpAggregator.Config;
 using Web.SkilledWorkers.HttpAggregator.Infrastructure;
 using Web.SkilledWorkers.HttpAggregator.Services;
@@ -34,6 +38,17 @@ namespace Web.SkilledWorkers.HttpAggregator
 			services.AddHttpClientForService("profileService", serviceUrlsConfig.ProfilesApi);
 			services.AddHttpClientForService("skillsService", serviceUrlsConfig.SkillsApi);
 
+			// Register the Swagger generator, defining 1 or more Swagger documents
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Skilled Workers API", Version = "v1" });
+
+				// Set the comments path for the Swagger JSON and UI.
+				var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+				var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+				c.IncludeXmlComments(xmlPath);
+			});
+
 			services.AddAutoMapper(config =>
 			{
 				config.AddProfile<MappingProfile>();
@@ -50,8 +65,18 @@ namespace Web.SkilledWorkers.HttpAggregator
 			{
 				app.UseDeveloperExceptionPage();
 			}
-
 			app.UseHttpsRedirection();
+
+			// Enable middleware to serve generated Swagger as a JSON endpoint.
+			app.UseSwagger();
+
+			// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+			// specifying the Swagger JSON endpoint.
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "Skilled Workers API");
+				c.RoutePrefix = string.Empty;
+			});
 
 			app.UseRouting();
 
